5863aa1be61fbf1d524ff9a71192af6a1e2c4c90
Harsh Johar <hpsj3112@gmail.com>
2022-03-17 05:51:03 +0530

    companies profile page

---
+++ components/CompanySections/KeyPeople.js
@@ -0,0 +1,23 @@
+function KeyPeople() {
+    return (
+        <div>
+            <div className="flex flex-col space-y-6 flex-1 py-10 justify-end relative">
+                <p className="text-[#62d5bc] text-[12px] font-bold cursor-pointer right-8 absolute top-8">
+                    +Add new members
+                </p>
+
+                <div className="flex items-center mb-2 px-2">
+                  <div className="grid gap-x-2 grid-cols-5">
+                    <input type="text" placeholder="Name" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
+                    <input type="text" placeholder="Role" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
+                    <input type="text" placeholder="Email" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
+                    <input type="text" placeholder="Alternate Email" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
+                    <input type="text" placeholder="LinkedIn" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
+                  </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default KeyPeople;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Main from "../components/Main";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 export default function Home() {
     return (
@@ -13,7 +13,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="main">
+            <div>
                 <Sidebar />
                 <Main />
             </div>
