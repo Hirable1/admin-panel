f054b4238e14ca189dbf2b873621b3a7c0329f0d
Harsh Johar <hpsj3112@gmail.com>
2022-03-18 15:00:25 +0530

    key people form

--- components/CompanySections/KeyPeople.js
+++ components/CompanySections/KeyPeople.js
@@ -1,19 +1,19 @@
+import { useState } from "react";
+
 function KeyPeople() {
+    const [people, setPeople] = useState([""]);
     return (
         <div>
             <div className="flex flex-col space-y-6 flex-1 py-10 justify-end relative">
-                <p className="text-[#62d5bc] text-[12px] font-bold cursor-pointer right-8 absolute top-8">
+                <p className="text-[#62d5bc] text-[12px] font-bold cursor-pointer right-8 absolute top-8" onClick={()=>setPeople([...people, ""])}>
                     +Add new members
                 </p>
 
-                <div className="flex items-center mb-2 px-2">
-                  <div className="grid gap-x-2 grid-cols-5">
-                    <input type="text" placeholder="Name" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
-                    <input type="text" placeholder="Role" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
-                    <input type="text" placeholder="Email" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
-                    <input type="text" placeholder="Alternate Email" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
-                    <input type="text" placeholder="LinkedIn" className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in" />
-                  </div>
+                <div className="flex items-center mb-2 px-2 flex-col space-y-2">
+                    {people.map((item) => (
+                        <PeopleForm key={item.id} />
+                    ))}
+                    <PeopleForm />
                 </div>
             </div>
         </div>
@@ -21,3 +21,35 @@ function KeyPeople() {
 }
 
 export default KeyPeople;
+
+const PeopleForm = () => {
+    return (
+        <div className="grid gap-x-2 grid-cols-5">
+            <input
+                type="text"
+                placeholder="Name"
+                className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in"
+            />
+            <input
+                type="text"
+                placeholder="Role"
+                className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in"
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in"
+            />
+            <input
+                type="text"
+                placeholder="Alternate Email"
+                className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in"
+            />
+            <input
+                type="text"
+                placeholder="LinkedIn"
+                className="px-3 py-2 placeholder-[#6B7280] text-[#030303]  placeholder-opacity-90 relative bg-white rounded text-sm border-[1.5px]  focus:outline-none focus:border-[#2dc5a1] focus:border-2  w-full  transition duration-200 ease-in"
+            />
+        </div>
+    );
+};
